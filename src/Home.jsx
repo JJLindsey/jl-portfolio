419f91f6a8f21cf50139000bcf25956d1de135f0
j_lindsey <jlindsey010@outlook.com>
2024-09-27 15:56:50 -0700

    fix text home

--- src/Home.jsx
+++ src/Home.jsx
@@ -4,52 +4,22 @@ import CodeIcon from '@mui/icons-material/Code';
 import CloseTag from './assets/icons8-code-50.png'
 import bauhaus from './assets/bauhausBack2.png'
 
-import { keyframes } from '@emotion/react';
-import styled from '@emotion/styled/macro';
+//import { keyframes } from '@emotion/react';
+//import styled from '@emotion/styled/macro';
 import {motion, AnimatePresence} from 'framer-motion'
 import Footer from './Components/Footer';
 
-const shimmerAnimation = keyframes`
- 0% {
-    background-position: 0% 50%;
-  }
-  100% {
-    background-position: 100% 50%;
-  }
-`
-
-const ShimmerTypography = styled('div')(({ showShimmer}) => ({
-    backgroundImage: showShimmer
-    ? 'linear-gradient(90deg, rgba(255,255,255,0) 0%, rgba(255,255,255,0.5) 50%, rgba(255,255,255,0) 100%), linear-gradient(to right, #002d66, #363276, #5d3481, #823385, #a53182, #c53379, #df3d6b, #f25058, #fd6a43, #ff8727)'
-    : 'linear-gradient(to right, #002d66, #363276, #5d3481, #823385, #a53182, #c53379, #df3d6b, #f25058, #fd6a43, #ff8727)',
-  backgroundSize: '200% 100%',
-  backgroundClip: 'text',
-  WebkitBackgroundClip: 'text',
-  fontSize: '50px',
-  color: 'transparent',
-  animation: showShimmer ? `${shimmerAnimation} 4s linear infinite` : 'none',
-  display: 'flex',
-  alignItems: 'center',
-}))
 
 export default function Home() {
-    const [showShimmer, setShowShimmer] = useState(true)
     const [word, setWord] = useState('code')
       
     useEffect(() => {
         const interval = setInterval(() => {
         setWord((prev) => (prev === 'code' ? 'craft' : 'code'));
-        }, 3000) // Change word every 2 seconds
+        }, 3000)
     
         return () => clearInterval(interval);
     }, [])
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setShowShimmer(false)
-    }, 5000)
-    return () => clearTimeout(timer)
-  }, [])
 
   return (
    <>
@@ -58,7 +28,6 @@ export default function Home() {
          
         sx={(theme) => ({
             width: '100%',
-            //height: '100vh',
             height: 'calc(100vh - 70px)',
             backgroundImage: `url(${bauhaus})`,
             backgroundSize: 'cover',
@@ -70,7 +39,6 @@ export default function Home() {
         <Container
             sx={{
             display: 'flex',
-            // flexDirection: 'column',
             justifyContent: 'flex-end',
             pt: { xs: 14, sm: 20 },
             pb: { xs: 8, sm: 12 },
@@ -83,12 +51,12 @@ export default function Home() {
                     <Typography variant='h3'  fontFamily="Noto Sans" fontWeight="600" >JENNIFER LINDSEY</Typography>
                 </Grid>
                 <Grid item>
-                    <ShimmerTypography
-                    showShimmer={showShimmer}
+                    <Typography
                     component="span"
-                    variant="h2"
+                    variant="h3"
+                    sx={{display: "flex", alignContent: "center"}}
                     >
-                    <CodeIcon sx={{fontSize: '50px', color: '#000', paddingTop: '15px'}}/>
+                    <CodeIcon sx={{fontSize: '50px', color: '#000', paddingTop: '5px'}}/>
                             not just{' '}
                         <AnimatePresence mode="wait">
                             <motion.span
@@ -116,7 +84,7 @@ export default function Home() {
                             </motion.span>
                         </AnimatePresence>   
                         <img src={CloseTag} style={{ color: '#000', paddingTop: '5px', paddingLeft: '10px' }} height={50} alt='icon'/>
-                    </ShimmerTypography>
+                    </Typography>
                 </Grid>
                     <Grid item>
                     <Typography variant="h5" textAlign="left" color="#000" style={{paddingTop: "32px", zIndex: 2}}>
@@ -135,7 +103,7 @@ export default function Home() {
       <Box
         sx={{
           width: '100%',
-          height: '50px', // Footer height
+          height: '50px',
           position: 'fixed',
           bottom: 0,
           left: 0,
