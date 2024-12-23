d4f1046d54e443646f97e1a6111bbd42e83361c4
jjlindsey <jlindsey010@gmail.com>
2024-12-23 12:27:53 -0800

    start adding project details

--- src/Components/Projects/ThreeSixtySessions.jsx
+++ src/Components/Projects/ThreeSixtySessions.jsx
@@ -1,15 +1,19 @@
-import { Grid, Typography } from '@mui/material'
+import { Card, Grid, Typography } from '@mui/material'
 import React from 'react'
 
 export default function ThreeSixtySessions() {
   return (
     <div>
       <Grid>
+        <Typography>Project Details</Typography>
         <Grid item>
           <Typography>360 Sessions - Atlanta-based Music Producer</Typography>
         </Grid>
         <Grid item>
           <Typography>Development</Typography>
+          <Card>
+            {/*project overview, tasks, what I solved */}
+          </Card>
         </Grid>
       </Grid>
     </div>
